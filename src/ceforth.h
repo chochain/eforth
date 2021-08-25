0d85b70f8b54679ebe24873c6a835158c524f32d
gnii <chochain@yahoo.com>
2021-08-24 19:00:01 -0700

    ~/src/cef.h add analogWrite for ESP32

--- src/ceforth.h
+++ src/ceforth.h
@@ -13,12 +13,13 @@
 
 #if ARDUINO
 #include <Arduino.h>
+#include <analogWrite.h>
 #define to_string(i)    string(String(i).c_str())
 #else
 #include <chrono>
 #include <thread>
 #define millis()        chrono::duration_cast<chrono::milliseconds>( \
-							chrono::steady_clock::now().time_since_epoch()).count()
+                            chrono::steady_clock::now().time_since_epoch()).count()
 #define delay(ms)       this_thread::sleep_for(chrono::milliseconds(ms))
 #define yield()         this_thread::yield()
 #endif // ARDUINO
@@ -40,23 +41,24 @@ struct ForthList {          /// vector helper template class
         if (v.empty()) throw underflow_error("ERR: stack empty");
         T t = v.back(); v.pop_back(); return t;
     }
-    int  size()               { (int)v.size();  }
+    int  size()               { return (int)v.size(); }
     void push(T t)            { v.push_back(t); }
     void clear()              { v.clear(); }
     void merge(ForthList& a)  { v.insert(v.end(), a.v.begin(), a.v.end()); }
+    void merge(vector<T>& v2) { v.insert(v.end(), v2.begin(), v2.end()); }
     void erase(int i)         { v.erase(v.begin() + i, v.end()); }
 };
 
 class Code;
 #if NO_FUNCTION
 struct fop {                                /// alternate solution for function
-	virtual void operator()(Code*) = 0;
+    virtual void operator()(Code*) = 0;
 };
 template<typename F>
 struct XT : fop {
-	F fp;
-	XT(F &f) : fp(f) {}
-	virtual void operator()(Code *c) { fp(c); }
+    F fp;
+    XT(F &f) : fp(f) {}
+    virtual void operator()(Code *c) { fp(c); }
 };
 #else
 using fop = function<void(Code*)>;         /// Forth operator
@@ -83,7 +85,7 @@ public:
 
 #if NO_FUNCTION
     template<typename F>
-    Code(string n, F fn, bool im=false);	/// primitive
+    Code(string n, F fn, bool im=false);    /// primitive
 #else
     Code(string n, fop fn, bool im=false);  /// primitive
 #endif // NO_FUNCTION
@@ -104,7 +106,7 @@ public:
 class ForthVM {
 public:
     istream          &cin;                  /// stream input
-	ostream          &cout;					/// stream output
+    ostream          &cout;                 /// stream output
 
     ForthList<DTYPE> rs;                    /// return stack
     ForthList<DTYPE> ss;                    /// parameter stack
@@ -123,11 +125,11 @@ public:
 private:
     DTYPE POP();
     DTYPE PUSH(DTYPE v);
-    
+
     Code *find(string s);                   /// search dictionary reversely
     string next_idiom(char delim=0);
     void call(Code *c);                     /// execute a word
-    
+
     void dot_r(int n, DTYPE v);
     void ss_dump();
     void words();
