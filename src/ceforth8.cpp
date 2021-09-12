521914d3cd04a908dfd2b8a344179f67bca6be98
gnii <chochain@yahoo.com>
2021-09-11 19:57:14 -0700

    ~/src/ceforth8 add dotstr; add comments

--- src/ceforth8.cpp
+++ src/ceforth8.cpp
@@ -2,20 +2,26 @@
 #include <stdlib.h>		// strtol
 #include <string.h>		// strcmp
 #include <exception>	// try...catch, throw
-
+///
+/// macro for alignment and standard units
+///
 #define ALIGN2(sz)		((sz) + (-(sz) & 0x1))
 #define ALIGN4(sz)		((sz) + (-(sz) & 0x3))
-
 typedef uint32_t U32;
 typedef uint16_t U16;
 typedef uint8_t  U8;
-
+///
+/// array class template (so we don't have dependency on C++ STL)
+/// Note:
+///   * using decorator pattern
+///   * this is similar to vector class but much simplified
+///
 template<class T, int N>
-struct List {          /// array storage helper template class
-    T   v[N];          /// using decorator pattern
-    int idx = 0;
+struct List {
+    T   v[N];           /// fixed array storage
+    int idx = 0;		/// current index of array
 
-    T& operator[](int i)   { return i < 0 ? v[N + i] : v[i]; }
+    T& operator[](int i)   { return i < 0 ? v[idx + i] : v[i]; }
     T pop() {
     	if (idx>0) return v[idx--];
     	throw "ERR: List empty";
@@ -29,7 +35,7 @@ struct List {          /// array storage helper template class
     void clear(int i=0)     { idx=i; }
 };
 ///
-/// functor implementation - for lambda support
+/// functor implementation - for lambda support (without STL)
 ///
 struct fop { virtual void operator()(int) = 0; };
 template<typename F>
@@ -38,7 +44,12 @@ struct XT : fop {			// universal functor
     XT(F &f) : fp(f) {}
     void operator()(int c) { fp(c); }
 };
-struct Code {				// 12-byte
+///
+/// universal Code class
+/// Note:
+///   * 12-byte on 32-bit machine, 24-byte on 64-bit machine
+///
+struct Code {
     const char *name = 0;	// name field
     fop *xt = 0;			// lambda pointer
     U16 def:  1;			// colon defined word
@@ -54,10 +65,10 @@ struct Code {				// 12-byte
 };
 ///
 /// main storages in RAM
-///
-/// by separating heap from dictionary, it makes dictionary uniform size
-/// (i.e. the RISC vs CISC debate) which eliminate the need for link field
-/// however, it require size tuning manually 
+/// Note:
+///   * by separating heap from dictionary, it makes dictionary uniform size
+///   * (i.e. the RISC vs CISC debate) which eliminate the need for link field
+///   * however, it require size tuning manually
 ///
 List<int,   64>      rs;
 List<int,   64>      ss;
@@ -74,7 +85,7 @@ int  WP = 0, IP = 0;
 ///
 int find(const char *s) {
     for (int i = dict.idx - (compile ? 2 : 1); i >= 0; --i) {
-        if (strcmp(s, dict[i]->name)) return i;
+        if (strcmp(s, dict[i]->name)==0) return i;
     }
     return -1;
 }
@@ -85,7 +96,7 @@ int find(const char *s) {
 ///   * if they are combined then can behaves similar to classic Forth
 /// 
 void colon(const char *name) {
-    const char *nfd = (const char*)&heap[heap.idx];    // current heap pointer
+	const char *nfd = (const char*)&heap[heap.idx];      // current heap pointer
     dict.push(new Code(nfd, [](int){}));    // create a new word on dictionary
     dict[-1]->def = 1;                      // specify a colon word
     dict[-1]->len = 0;                		// advance counter (by number of U16)
@@ -96,17 +107,17 @@ void colon(const char *name) {
 };
 void addcode(U16 c) {                       // add an opcode to pf 
 	heap.push((U8*)&c, sizeof(U16));
-	dict[-1]->len += 2;                     // advance for 16-bit integer
+	dict[-1]->len += sizeof(U16);           // advance for 16-bit integer
 }
 void addvar(int n) {                        // add a dovar
     addcode(find("dovar"));                 // dovar,
     heap.push((U8*)&n, sizeof(int));        // int
-    dict[-1]->len += 4;                   	// advance for a 32-bit integer
+    dict[-1]->len += size(U32);           	// advance for a 32-bit integer
 }
-void addlit(const char *s) {                // add a string
-    int sz = ALIGN2(strlen(s)+1);
+void addstr(const char *s) {                // add a string
+    int sz = ALIGN2(strlen(s)+1);           // padding to 2-byte align
     addcode(find("dotstr"));                // dostr,
-    heap.push((U8*)s, sz);                  // byte, byte, byte, ...
+    heap.push((U8*)s, sz);                  // byte0, byte1, byte2, ..., byteN
     dict[-1]->len += sz;                    // skip to next opcode
 }    
 void nest(int c) {
@@ -118,8 +129,9 @@ void nest(int c) {
     try {
         int n = w->len;						// how many
     	while (IP < n) {
-    		int i = heap[w->pf + IP++];		// fetch instruction from heap space
+    		int i = heap[w->pf + IP];		// fetch instruction from heap space
     		nest(i);
+            IP += sizeof(U16);              // advance to next instruction
     	}
     }
     catch(...) {}
@@ -136,14 +148,18 @@ string strbuf;
 ///
 /// debug functions
 ///
+void ss_dump() {
+	cout << " <"; for (int i=0; i<ss.idx; i++) { cout << ss[i] << " "; }
+	cout << top << "> ok" << ENDL;
+}
 void see(int c) {
-
+	// TODO
 }
 void words() {
 	for (int i=dict.idx - 1; i>=0; i--) {
 		Code *w = dict[i];
-		if ((i%10)==0) cout << ENDL;
 		cout << w->name << " " << i << (w->immd ? "* " : " ");
+		if ((i%10)==0) cout << ENDL;
 	}
 }
 ///
@@ -173,17 +189,26 @@ static Code prim[] = {
     CODE("swap", int n = ss.pop(); PUSH(n)),
     CODE("+",    top += ss.pop()),
     CODE("-",    top =  ss.pop() - top),
+    CODE(".",       cout << POP() << " "),
     CODE("dotstr",
-         const char *s = (char*)&heap[dict[WP]->pf+IP+1];
+         int x = dict[WP]->pf+IP+sizeof(U16);
+         const char *s = (char*)&heap[i];
          cout << s;
-         IP += ALIGN2(strlen(s))),
+         IP += ALIGN2(strlen(s)+1)),
+    CODE("dolit",
+         int x  = dict[WP]->pf+IP+sizeof(U16);
+         int *i = (int*)&heap[x];
+         PUSH(*i); IP += sizeof(int)),
     CODE("dovar",
-         int *i = (int*)&heap[dict[WP]->pf+IP+1];
-         PUSH(*i); IP += 2),
-    CODE(":", _colon),
-//    	cin >> strbuf;
-//    	colon(strbuf.c_str());
-//    	compile=true),
+         int x  = dict[WP]->pf+IP+sizeof(U16);
+         PUSH(x); IP += sizeof(int)),
+    IMMD(".\"",
+         getline(cin, strbuf, '"');
+         addstr(strbuf.substr(1).c_str())),
+    CODE(":",
+    	cin >> strbuf;
+    	colon(strbuf.c_str());
+    	compile=true),
     IMMD(";", compile = false),
     CODE("words", words())
 };
@@ -208,7 +233,7 @@ int outer() {
 	    // try as a number
 	    char *p;
 	    int n = static_cast<int>(strtol(idiom, &p, base));
-	    //printf("%d\n", n);
+	    printf("%d\n", n);
 	    if (*p != '\0') {                           /// * not number
 	    	cout << idiom << "? " << ENDL;          ///> display error prompt
 	        compile = false;                        ///> reset to interpreter mode
@@ -218,18 +243,18 @@ int outer() {
 	    if (compile) addvar(n);                     /// * a number in compile mode?
 	    else PUSH(n);                               ///> or, add value onto data stack
 	}
-	if (!compile) {
-		cout << " <"; for (int i=0; i<ss.idx; i++) { cout << ss[i] << " "; }
-		cout << top << "> ok" << ENDL;
-	}
+	if (!compile) ss_dump();
 	return 1;
 }
-int main(int ac, char* av[]) {
-	cout << unitbuf << "eForth8" << ENDL;
-	for (int i=0; i<PSZ; i++) {
-		dict.push(&prim[i]);	/// build dictionary
-	}
+void init() {
+	for (int i=0; i<PSZ; i++) {						/// copy prim(ROM) into RAM dictionary,
+		dict.push(&prim[i]);		                /// we don't need to do this if modify
+	}                                               /// find to support both
 	words();
+}
+int main(int ac, char* av[]) {
+	init();
+	cout << unitbuf << "eForth8" << ENDL;
 	while (outer());
 	cout << "Done." << ENDL;
 	return 0;
