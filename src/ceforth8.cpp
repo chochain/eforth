8cde0e6fa83633115a53f6517fcb5e54965ac596
gnii <chochain@yahoo.com>
2021-09-12 13:57:26 -0700

    ~/src/ceforth8 rename heap=>pmem; add sstream fin, fout; nest() no auto var

--- src/ceforth8.cpp
+++ src/ceforth8.cpp
@@ -1,7 +1,10 @@
-#include <stdint.h>		// uintxx_t
-#include <stdlib.h>		// strtol
-#include <string.h>		// strcmp
-#include <exception>	// try...catch, throw
+///
+/// ceForth8 experimental
+///
+#include <stdint.h>     // uintxx_t
+#include <stdlib.h>     // strtol
+#include <string.h>     // strcmp
+#include <exception>    // try...catch, throw
 ///
 /// logical units (instead of physical) for type check and portability
 ///
@@ -9,11 +12,11 @@ typedef uint16_t IU;    // instruction pointer unit
 typedef int32_t  DU;    // data unit
 typedef uint16_t U16;   // unsigned 16-bit integer
 typedef uint8_t  U8;    // byte, unsigned character
-#define ALIGN(sz)		((sz) + (-(sz) & 0x1))     
-//#define ALIGN4(sz)		((sz) + (-(sz) & 0x3))
 ///
 /// alignment macros
 ///
+#define ALIGN(sz)       ((sz) + (-(sz) & 0x1))
+//#define ALIGN4(sz)        ((sz) + (-(sz) & 0x3))
 ///
 /// array class template (so we don't have dependency on C++ STL)
 /// Note:
@@ -22,17 +25,17 @@ typedef uint8_t  U8;    // byte, unsigned character
 ///
 template<class T, int N>
 struct List {
-    T   v[N];        	/// fixed-size array storage
-    int idx = 0;		/// current index of array
+    T   v[N];           /// fixed-size array storage
+    int idx = 0;        /// current index of array
 
     T& operator[](int i)   { return i < 0 ? v[idx + i] : v[i]; }
     T pop() {
-    	if (idx>0) return v[--idx];
-    	throw "ERR: List empty";
+        if (idx>0) return v[--idx];
+        throw "ERR: List empty";
     }
     T push(T t) {
-    	if (idx<N) return v[idx++] = t;
-    	throw "ERR: List full";
+        if (idx<N) return v[idx++] = t;
+        throw "ERR: List full";
     }
     void push(T *a, int n)  { for (int i=0; i<n; i++) push(*(a+i)); }
     void merge(List& a)     { for (int i=0; i<a.idx; i++) push(a[i]);}
@@ -43,7 +46,7 @@ struct List {
 ///
 struct fop { virtual void operator()(IU) = 0; };
 template<typename F>
-struct XT : fop {			// universal functor
+struct XT : fop {           // universal functor
     F fp;
     XT(F &f) : fp(f) {}
     void operator()(IU c) { fp(c); }
@@ -54,36 +57,36 @@ struct XT : fop {			// universal functor
 ///   * 8-byte on 32-bit machine, 16-byte on 64-bit machine
 ///
 struct Code {
-    const char *name = 0;	/// name field
+    const char *name = 0;   /// name field
     union {                 /// either a primitive or colon word
-    	fop *xt = 0;		/// lambda pointer
-    	struct {            /// a colon word
-    		U16 def:  1;	/// colon defined word
-    		U16 immd: 1;	/// immediate flag
-    		U16 len:  14;	/// len of pf
-    		IU  pf;			/// offset to heap space
-    	};
+        fop *xt = 0;        /// lambda pointer
+        struct {            /// a colon word
+            U16 def:  1;    /// colon defined word
+            U16 immd: 1;    /// immediate flag
+            U16 len:  14;   /// len of pf
+            IU  pf;         /// offset to pmem space
+        };
     };
     template<typename F>    /// template function for lambda
     Code(const char *n, F f, bool im=false) : name(n) {
-    	xt = new XT<F>(f);
-    	immd = im ? 1 : 0;
+        xt = new XT<F>(f);
+        immd = im ? 1 : 0;
     }
 };
 ///
 /// main storages in RAM
 /// Note:
-///   1.By separating heap from dictionary, it makes dictionary uniform size
-///   * (i.e. the RISC vs CISC debate) which eliminate the need for link field
-///   * however, it require size tuning manually
-///   2.For ease of byte counting, we use U8 for heap instead of U16.
+///   1.By separating pmem from dictionary, it makes dictionary uniform size
+///   * (i.e. the RISC vs CISC debate) which eliminates the need for link field
+///   * however, it requires size tuning manually
+///   2.For ease of byte counting, we use U8 for pmem instead of U16.
 ///   * this makes IP increment by 2 instead of word size. If needed, it can be
 ///   * readjusted.
 ///
-List<DU,    64>       ss;   // data stack, can reside in registers for some processors
-List<DU,    64>       rs;   // return stack
-List<Code*, 1024>    dict;	// fixed sized dictionary (RISC vs CISC)
-List<U8,    48*1024> heap;	// storage for all colon definitions
+List<DU,    64>      ss;   /// data stack, can reside in registers for some processors
+List<DU,    64>      rs;   /// return stack
+List<Code*, 1024>    dict; /// fixed sized dictionary (RISC vs CISC)
+List<U8,    48*1024> pmem; /// storage for all colon definitions
 ///
 /// system variables
 ///
@@ -102,57 +105,60 @@ int find(const char *s) {
 ///
 /// colon word compiler
 /// Note:
-///   * we separate dict and heap space to make word uniform in size
+///   * we separate dict and pmem space to make word uniform in size
 ///   * if they are combined then can behaves similar to classic Forth
 ///   * with an addition link field added.
-/// 
+///
 void colon(const char *name) {
-	const char *nfd = (const char*)&heap[heap.idx];      // current heap pointer
+    const char *nfd = (const char*)&pmem[pmem.idx];      // current pmem pointer
     dict.push(new Code(nfd, [](int){}));    // create a new word on dictionary
     dict[-1]->def = 1;                      // specify a colon word
-    dict[-1]->len = 0;                		// advance counter (by number of U16)
+    dict[-1]->len = 0;                      // advance counter (by number of U16)
     int sz = ALIGN(strlen(name)+1);         // IU (16-bit) alignment
-    heap.push((U8*)name, sz); 		        // setup raw name field
-    dict[-1]->pf  = heap.idx;               // capture code field index
+    pmem.push((U8*)name, sz);               // setup raw name field
+    dict[-1]->pf  = pmem.idx;               // capture code field index
 };
 void addcode(IU c) {
-	heap.push((U8*)&c, sizeof(IU));         // add an opcode to pf
-	dict[-1]->len += sizeof(IU);            // advance by instruction size
+    pmem.push((U8*)&c, sizeof(IU));         // add an opcode to pf
+    dict[-1]->len += sizeof(IU);            // advance by instruction size
 }
 void addvar() {                             // add a dovar (variable)
     DU n = 0;                               // default variable value
     addcode(find("dovar"));                 // dovar (+parameter field)
-    heap.push((U8*)&n, sizeof(DU));         // data storage (32-bit integer now)
-    dict[-1]->len += sizeof(DU);           	// skip to next field
+    pmem.push((U8*)&n, sizeof(DU));         // data storage (32-bit integer now)
+    dict[-1]->len += sizeof(DU);            // skip to next field
 }
 void addlit(DU n) {                         // add a dolit (constant)
     addcode(find("dolit"));                 // dovar (+parameter field)
-    heap.push((U8*)&n, sizeof(DU));         // data storage (32-bit integer now)
-    dict[-1]->len += sizeof(DU);           	// skip to next field
+    pmem.push((U8*)&n, sizeof(DU));         // data storage (32-bit integer now)
+    dict[-1]->len += sizeof(DU);            // skip to next field
 }
 void addstr(const char *s) {                // add a string
     int sz = ALIGN(strlen(s)+1);            // IU (16-bit) alignment
     addcode(find("dotstr"));                // dostr, (+parameter field)
-    heap.push((U8*)s, sz);                  // byte0, byte1, byte2, ..., byteN
+    pmem.push((U8*)s, sz);                  // byte0, byte1, byte2, ..., byteN
     dict[-1]->len += sz;                    // skip to next field
 }
 ///
 /// Forth inner interpreter
 ///
+int maxbss = 0;
 void nest(IU c) {
-    Code *w = dict[c];						// get word from dictionary
-    if (!w->def) {							// is a primitive?
-    	(*(fop*)(((uintptr_t)w->xt)&~0x3))(c);	// mask out immd (and def), and execute
+    ///
+    /// by not using any temp variable here can prevent auto stack allocation
+    ///
+    if (!dict[c]->def) {                    // is a primitive?
+        (*(fop*)(((uintptr_t)dict[c]->xt)&~0x3))(c);  // mask out immd (and def), and execute
         return;
-    } 
+    }
     // is a colon words
     rs.push(WP); rs.push(IP); WP=c; IP=0;   // setup call frame
+    if (rs.idx > maxbss) maxbss = rs.idx;   // keep rs sizing matrics
     try {
-    	while (IP < w->len) {               // in instruction range
-    		IU i = heap[w->pf + IP];		// fetch instruction from heap space
-    		nest(i);                        // call recursively
+        while (IP < dict[c]->len) {         // in instruction range
+            nest(pmem[dict[c]->pf + IP]);   // fetch/exec instruction from pmem space
             IP += sizeof(IU);               // advance to next instruction
-    	}
+        }
     }
     catch(...) {}
     IP = rs.pop(); WP = rs.pop();           // restore call frame
@@ -160,34 +166,36 @@ void nest(IU c) {
 ///
 /// utilize C++ standard template libraries for core functions only
 ///
-#include <iostream>     // cin, cout
-#include <string>		// string class
+#include <sstream>     // istringstream, ostringstream, cin, cout
+#include <string>      // string class
 using namespace std;
 #define ENDL    endl
+istringstream   fin;   /// forth_in
+ostringstream   fout;  /// forth_out
 string strbuf;
 ///
 /// debug functions
 ///
 void ss_dump() {
-	cout << " <"; for (int i=0; i<ss.idx; i++) { cout << ss[i] << " "; }
-	cout << top << "> ok" << ENDL;
+    fout << " <"; for (int i=0; i<ss.idx; i++) { fout << ss[i] << " "; }
+    fout << top << "> ok" << ENDL;
 }
 void see(IU c) {
-	// TODO
+    // TODO
 }
 void words() {
-	for (int i=dict.idx - 1; i>=0; i--) {
-		Code *w = dict[i];
-		cout << w->name << " " << i << (w->immd ? "* " : " ");
-		if ((i%10)==0) cout << ENDL;
-	}
+    for (int i=dict.idx - 1; i>=0; i--) {
+        Code *w = dict[i];
+        fout << w->name << " " << i << (w->immd ? "* " : " ");
+        if ((i%10)==0) fout << ENDL;
+    }
 }
 ///
 /// macros to reduce verbosity
 ///
 inline DU   PUSH(DU v) { ss.push(top); return top = v;         }
 inline DU   POP()      { DU n=top; top=ss.pop(); return n;     }
-inline void WORD()     { cin >> strbuf; colon(strbuf.c_str()); }  // create a colon word
+inline void WORD()     { fin >> strbuf; colon(strbuf.c_str()); }  // create a colon word
 inline IU   PARAM()    { return (dict[WP]->pf+IP+sizeof(IU));  }  // get parameter field
 #define     CODE(s, g) { s, [&](IU c){ g; }, 0 }
 #define     IMMD(s, g) { s, [&](IU c){ g; }, 1 }
@@ -200,9 +208,9 @@ inline IU   PARAM()    { return (dict[WP]->pf+IP+sizeof(IU));  }  // get paramet
 ///   * find() needs to be modified to support ROM+RAM
 ///
 auto _colon = [&](int c) {
-	cin >> strbuf;
-	colon(strbuf.c_str());
-	compile=true;
+    fin >> strbuf;
+    colon(strbuf.c_str());
+    compile=true;
 };
 static Code prim[] = {
     CODE("dup",  PUSH(top)),
@@ -211,21 +219,21 @@ static Code prim[] = {
     CODE("swap", DU n = ss.pop(); PUSH(n)),
     CODE("+",    top += ss.pop()),
     CODE("-",    top =  ss.pop() - top),
-    CODE(".",    cout << POP() << " "),
+    CODE(".",    fout << POP() << " "),
     CODE("dovar",
-         IU x = PARAM();                     // get heap offset to parameter field
+         IU x = PARAM();                     // get pmem offset to parameter field
          PUSH(x); IP += sizeof(DU)),         // push and advance to next instruction
     CODE("dolit",
          IU x = PARAM();                     // parameter field
-         DU *i = (DU*)&heap[x];              // fetch the value
+         DU *i = (DU*)&pmem[x];              // fetch the value
          PUSH(*i); IP += sizeof(DU)),        // push and advance to next instruction
    CODE("dotstr",
-		 IU x = PARAM();                     // heap offset to parameter field
-         const char *s = (char*)&heap[x];    // get string pointer
-         cout << s;                          // send to output console
+         IU x = PARAM();                     // pmem offset to parameter field
+         const char *s = (char*)&pmem[x];    // get string pointer
+         fout << s;                          // send to output console
          IP += ALIGN(strlen(s)+1)),          // advance to next instruction
     IMMD(".\"",
-         getline(cin, strbuf, '"');
+         getline(fin, strbuf, '"');
          addstr(strbuf.substr(1).c_str())),
     CODE("variable", WORD(); addvar()),
     CODE("constant", WORD(); addlit(POP())),
@@ -238,45 +246,50 @@ const int PSZ = sizeof(prim)/sizeof(Code);
 ///
 /// outer interpreter
 ///
-int outer() {
-	while (cin >> strbuf) {
-		const char *idiom = strbuf.c_str();
-		// printf("%s=>", idiom);
-		int w = find(idiom);           /// * search through dictionary
-		if (w>=0) {                                 /// * word found?
-			// printf("%s\n", dict[w]->name);
-			if (compile && !dict[w]->immd) {        /// * in compile mode?
-				addcode(w);                         /// * add to colon word
-	        }
-	        else { nest(w); ss_dump(); }            /// * execute forth word
-	        continue;
-	    }
-	    // try as a number
-	    char *p;
-	    int n = static_cast<int>(strtol(idiom, &p, base));
-	    // printf("%d\n", n);
-	    if (*p != '\0') {                           /// * not number
-	    	cout << idiom << "? " << ENDL;          ///> display error prompt
-	        compile = false;                        ///> reset to interpreter mode
-	        break;                                  ///> skip the entire input buffer
-	    }
-	    // is a number
-	    if (compile) addlit(n);                     /// * add literal when in compile mode
-	    else { PUSH(n); ss_dump(); }                ///> or, add value onto data stack
-	}
-	return 1;
+int forth_outer() {
+    while (fin >> strbuf) {
+        const char *idiom = strbuf.c_str();
+        // printf("%s=>", idiom);
+        int w = find(idiom);           /// * search through dictionary
+        if (w>=0) {                                 /// * word found?
+            // printf("%s\n", dict[w]->name);
+            if (compile && !dict[w]->immd) {        /// * in compile mode?
+                addcode(w);                         /// * add to colon word
+            }
+            else { nest(w); ss_dump(); }            /// * execute forth word
+            continue;
+        }
+        // try as a number
+        char *p;
+        int n = static_cast<int>(strtol(idiom, &p, base));
+        // printf("%d\n", n);
+        if (*p != '\0') {                           /// * not number
+            fout << idiom << "? " << ENDL;          ///> display error prompt
+            compile = false;                        ///> reset to interpreter mode
+            break;                                  ///> skip the entire input buffer
+        }
+        // is a number
+        if (compile) addlit(n);                     /// * add literal when in compile mode
+        else { PUSH(n); ss_dump(); }                ///> or, add value onto data stack
+    }
+    return 1;
 }
-void init() {
-	for (int i=0; i<PSZ; i++) {						/// copy prim(ROM) into RAM dictionary,
-		dict.push(&prim[i]);		                /// we don't need to do this if modify
-	}                                               /// find to support both
-	words();
+void forth_init() {
+    for (int i=0; i<PSZ; i++) {                     /// copy prim(ROM) into RAM dictionary,
+        dict.push(&prim[i]);                        /// we don't need to do this if modify
+    }                                               /// find to support both
+    words();
 }
 int main(int ac, char* av[]) {
-	init();
-	cout << unitbuf << "eForth8" << ENDL;
-	while (outer());
-	cout << "Done." << ENDL;
-	return 0;
+    forth_init();
+    cout << unitbuf << "eForth8" << ENDL;
+    while (cin >> strbuf) {
+        fout.str("");
+        fin.clear();
+        fin.str(strbuf.c_str());
+        forth_outer();
+        cout << fout.str();
+    }
+    cout << "Done." << ENDL;
+    return 0;
 }
-
