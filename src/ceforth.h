c0952387f7b31131eb58acd8ad3d44c6cd4260e7
gnii <chochain@yahoo.com>
2022-05-18 20:00:08 -0700

    ~/src/ceforth.h reorg to sync with esp32forth

--- src/ceforth.h
+++ src/ceforth.h
@@ -103,7 +103,9 @@ struct List {
     void clear(int i=0)    INLINE { idx=i; }
 };
 ///
-/// functor implementation - for lambda support (without STL)
+/// universal functor (no STL) and Code class
+/// Note:
+///   * 8-byte on 32-bit machine, 16-byte on 64-bit machine
 ///
 #if LAMBDA_OK
 struct fop { virtual void operator()() = 0; };
@@ -113,16 +115,6 @@ struct XT : fop {           // universal functor
     XT(F &f) : fp(f) {}
     void operator()() INLINE { fp(); }
 };
-#else  // LAMBDA_OK
-typedef void (*fop)();
-#endif // LAMBDA_OK
-
-///
-/// universal Code class
-/// Note:
-///   * 8-byte on 32-bit machine, 16-byte on 64-bit machine
-///
-#if LAMBDA_OK
 struct Code {
     const char *name = 0;   /// name field
     union {                 /// either a primitive or colon word
@@ -142,6 +134,7 @@ struct Code {
     Code() {}               /// create a blank struct (for initilization)
 };
 #else  // LAMBDA_OK
+typedef void (*fop)();      /// function pointer
 struct Code {
     const char *name = 0;   /// name field
     union {                 /// either a primitive or colon word
@@ -170,6 +163,7 @@ struct Code {
 #define BOOL(f)   ((f)?-1:0)
 #define PFA(w)    ((U8*)&pmem[dict[w].pfa]) /** parameter field pointer of a word        */
 #define HERE      (pmem.idx)                /** current parameter memory index           */
+#define XT(ipx)   (DICT0 + (S16)ipx)        /** fetch xt from dictionary offset          */
 #define OFF(ip)   ((IU)((U8*)(ip) - MEM0))  /** IP offset (index) in parameter memory    */
 #define MEM(ip)   (MEM0 + *(IU*)(ip))       /** pointer to IP address fetched from pmem  */
 #define CELL(a)   (*(DU*)&pmem[a])          /** fetch a cell from parameter memory       */
@@ -211,23 +205,6 @@ public:
 
 private:
     ///
-    /// compiler methods
-    ///
-    void add_iu(IU i) { pmem.push((U8*)&i, sizeof(IU));  dict[-1].len += sizeof(IU); }  /** add an instruction into pmem */
-    void add_du(DU v) { pmem.push((U8*)&v, sizeof(DU)),  dict[-1].len += sizeof(DU); }  /** add a cell into pmem         */
-    void add_str(const char *s) {                                               /** add a string to pmem         */
-        int sz = STRLEN(s); pmem.push((U8*)s,  sz); dict[-1].len += sz;
-    }
-    void add_w(IU w) {
-        Code *c  = &dict[w];
-        IU   ipx = c->def ? (c->pfa | 1) : (IU)((UFP)c->xt - DICT0);
-        add_iu(ipx);
-        printf("add_w(%d) => %4x:%p %s\n", w, ipx, c->xt, c->name);
-    }
-    string &next_idiom(char c=0);
-    void  colon(const char *name);
-    void  colon(string &s) { colon(s.c_str()); }
-    ///
     /// dictionary search methods
     ///
     int   pfa2word(U8 *ip);
@@ -235,15 +212,32 @@ private:
     int   find(const char *s);
     int   find(string &s) { return find(s.c_str()); }
     ///
-    /// VM ops
+    /// compiler methods
+    ///
+    void  colon(const char *name);
+    void  colon(string &s) { colon(s.c_str()); }
+    void  add_iu(IU i) { pmem.push((U8*)&i, sizeof(IU));  dict[-1].len += sizeof(IU); }  /** add an instruction into pmem */
+    void  add_du(DU v) { pmem.push((U8*)&v, sizeof(DU)),  dict[-1].len += sizeof(DU); }  /** add a cell into pmem         */
+    void  add_str(const char *s) {                                               /** add a string to pmem         */
+        int sz = STRLEN(s); pmem.push((U8*)s,  sz); dict[-1].len += sz;
+    }
+    void  add_w(IU w) {
+        Code *c  = &dict[w];
+        IU   ipx = c->def ? (c->pfa | 1) : (IU)((UFP)c->xt - DICT0);
+        add_iu(ipx);
+        printf("add_w(%d) => %4x:%p %s\n", w, ipx, c->xt, c->name);
+    }
+    ///
+    /// inner interpreter ops
     ///
     void  PUSH(DU v) { ss.push(top); top = v; }
     DU    POP()      { DU n = top; top = ss.pop(); return n; }
     void  call(IU w);
     void  nest();
     ///
-    /// debug methods
+    /// IO & debug methods
     ///
+    string &next_idiom(char c=0);
     void  dot_r(int n, int v);
     void  to_s(IU c);
     void  see(U8 *ip, int dp=1);
