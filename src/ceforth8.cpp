f9993cd8fe0df3ea7b78f02a84a4487856e6c200
gnii <chochain@yahoo.com>
2022-05-20 07:11:20 -0700

    ~/src/ceforth8.cpp use FPTR to unify function pointer and lamdba; sync with master

--- src/ceforth8.cpp
+++ src/ceforth8.cpp
@@ -27,10 +27,10 @@ using namespace std;
 ///
 /// conditional compililation options
 ///
-/// Note: lambda enable VM taking parameters (with capture)
-///       but nest() becomes 2x slower
+/// Note: lambda enables VM taking parameters (with capture)
+///       but slows nest() by 3%
 ///
-#define LAMBDA_OK       0
+#define LAMBDA_OK       1
 #define RANGE_CHECK     0
 #define INLINE          __attribute__((always_inline))
 ///
@@ -122,10 +122,11 @@ struct XT : fop {           // universal functor
     XT(F &f) : fp(f) {}
     void operator()() INLINE { fp(); }
 };
+typedef fop* FPTR;          // lambda function pointer
 struct Code {
     const char *name = 0;   /// name field
     union {                 /// either a primitive or colon word
-        fop *xt = 0;        /// lambda pointer
+        FPTR xt = 0;        /// lambda pointer
         struct {            /// a colon word
             U16 def:  1;    /// colon defined word
             U16 immd: 1;    /// immediate flag
@@ -146,11 +147,11 @@ struct Code {
 ///
 /// a lambda without capture can degenerate into a function pointer
 ///
-typedef void (*fop)();
+typedef void (*FPTR)();
 struct Code {
     const char *name = 0;   /// name field
     union {                 /// either a primitive or colon word
-        fop xt = 0;         /// lambda pointer
+        FPTR xt = 0;        /// lambda pointer
         struct {            /// a colon word
             U16 def:  1;    /// colon defined word
             U16 immd: 1;    /// immediate flag
@@ -158,7 +159,7 @@ struct Code {
             IU  pfa;        /// offset to pmem space (16-bit for 64K range)
         };
     };
-    Code(const char *n, fop f, bool im=false) : name(n), xt(f) {
+    Code(const char *n, FPTR f, bool im=false) : name(n), xt(f) {
         immd = im ? 1 : 0;
     }
     Code() {}               /// create a blank struct (for initilization)
@@ -273,7 +274,13 @@ void colon(const char *name) {
         dict[-1].name, dict[-1].name);
 };
 ///
-/// Forth inner interpreter - recursive (look nicer but use system stack)
+/// Forth inner interpreter
+///
+#define CALL(w) \
+    if (dict[w].def) { WP = w; IP = PFA(w); nest(); } \
+    else (*(FPTR)((UFP)dict[w].xt & ~0x3))()
+///
+/// recursive version (look nicer but use system stack)
 /// Note: superceded by iterator version below (~8% faster)
 /*
 void nest() {
@@ -291,24 +298,15 @@ void nest() {
         else {
             UFP xt = DICT0 + (*(IU*)IP & ~0x3);  /// * function pointer
             IP += sizeof(IU);                    /// * advance to next opcode
-            (*(fop*)xt)();
+            (*(FPTR)xt)();
         }
     }
     yield();                                ///> give other tasks some time
 }
 */
 ///
-/// Forth inner interpreter - interative (instead of recursive)
+/// interative version
 ///
-#if LAMBDA_OK
-#define CALL(w) \
-    if (dict[w].def) { WP = w; IP = PFA(w); nest(); } \
-    else (*(fop*)((UFP)dict[w].xt & ~0x3))()
-#else  // LAMBDA_OK
-#define CALL(w) \
-    if (dict[w].def) { WP = w; IP = PFA(w); nest(); } \
-    else (*(fop)((UFP)dict[w].xt & ~0x3))()
-#endif // LAMBDA_OK
 void nest() {
     int dp = 0;                                      /// iterator depth control
     while (dp >= 0) {
@@ -321,11 +319,7 @@ void nest() {
                 IP = MEM0 + (ipx & ~0x1);            /// word pfa (def masked)
                 dp++;                                /// one level deeper
             }
-#if LAMBDA_OK
-            else (*(fop*)(DICT0 + (ipx & ~0x3)))();  /// * execute primitive word
-#else  // LAMBDA_OK
-            else (*(fop)(DICT0 + (ipx & ~0x3)))();   /// * execute primitive word
-#endif // LAMBDA_OK
+            else (*(FPTR)(DICT0 + (ipx & ~0x3)))();  /// * execute primitive word
             ipx = *(IU*)IP;
         }
         if (dp-- > 0) {
@@ -419,7 +413,11 @@ void mem_dump(IU p0, DU sz) {
 inline char *next_idiom() { fin >> strbuf; return (char*)strbuf.c_str(); } // get next idiom
 inline char *scan(char c) { getline(fin, strbuf, c); return (char*)strbuf.c_str(); }
 inline DU   POP()         { DU n=top; top=ss.pop(); return n; }
-inline void PUSH(DU v)    { ss.push(top); top = v; }
+///
+/// This is a killer!!! 3400ms vs 1200ms per 100M cycles, TODO: why?
+/// inline PUSH(DU v)     { ss.push(top); top = v; }
+///
+#define     PUSH(v)       { ss.push(top); top = (v); }
 ///
 /// global memory access macros
 ///
