3989bfbb7cffa4bb5c91dbd8548d5e6ac21acbef
gnii <chochain@yahoo.com>
2022-05-28 14:11:34 -0700

    ~/src/ceforth.h update comments for doxygen

--- src/ceforth.h
+++ src/ceforth.h
@@ -4,8 +4,6 @@
 #include <sstream>
 #include <exception>        // try...catch, throw
 ///
-/// Conditional compililation options
-///
 /// Benchmark: 10K*10K cycles on desktop (3.2G AMD)
 ///    LAMBDA_OK       0 cut 80ms
 ///    RANGE_CHECK     0 cut 100ms
@@ -16,20 +14,22 @@
 ///    * it slow down nest() by 2x (1200ms -> 2500ms)
 ///    * with one parameter, it slows 160ms extra
 ///
-#define LAMBDA_OK       0    /** set 1 for ForthVM.this */
-#define RANGE_CHECK     0
-#define CC_DEBUG        0
+///@name Conditional compililation options
+///@}
+#define LAMBDA_OK       0     /**< lambda support, set 1 for ForthVM.this */
+#define RANGE_CHECK     0     /**< vector range check                     */
+#define CC_DEBUG        0     /**< debug tracing flag                     */
 #define INLINE          __attribute__((always_inline))
-///
-/// memory block configuation
-///
+///@}
+///@name Memory block configuation
+///@{
 #define E4_RS_SZ        64
 #define E4_SS_SZ        64
 #define E4_DICT_SZ      2048
 #define E4_PMEM_SZ      (64*1024)
-///
-/// multi-platform support
-///
+///@}
+///@name Multi-platform support
+///@{
 #if _WIN32 || _WIN64
 #define ENDL "\r\n"
 #else  // _WIN32 || _WIN64
@@ -54,11 +54,11 @@
 #define yield()         this_thread::yield()
 #define PROGMEM
 #endif // ARDUINO
-
+///@}
 using namespace std;
 ///
-/// logical units (instead of physical) for type check and portability
-///
+///@name Logical units (instead of physical) for type check and portability
+///@{
 typedef uint32_t        U32;   // unsigned 32-bit integer
 typedef uint16_t        U16;   // unsigned 16-bit integer
 typedef uint8_t         U8;    // byte, unsigned character
@@ -73,16 +73,16 @@ typedef int32_t         DU;
 #define DVAL            0
 #endif // USE_FLOAT
 typedef uint16_t        IU;    // instruction pointer unit
-///
-/// alignment macros
-///
+///@}
+///@name Alignment macros
+///@{
 #define ALIGN2(sz)      ((sz) + (-(sz) & 0x1))
 #define ALIGN4(sz)      ((sz) + (-(sz) & 0x3))
 #define ALIGN16(sz)     ((sz) + (-(sz) & 0xf))
 #define ALIGN32(sz)     ((sz) + (-(sz) & 0x1f))
 #define ALIGN(sz)       ALIGN2(sz)
 #define STRLEN(s)       (ALIGN(strlen(s)+1))  /** calculate string size with alignment */
-///
+///@}
 /// array class template (so we don't have dependency on C++ STL)
 /// Note:
 ///   * using decorator pattern
@@ -90,15 +90,15 @@ typedef uint16_t        IU;    // instruction pointer unit
 ///
 template<class T, int N=0>
 struct List {
-    T   *v;             /// fixed-size array storage
-    int idx = 0;        /// current index of array
-    int max = 0;        /// high watermark for debugging
+    T   *v;             ///< fixed-size array storage
+    int idx = 0;        ///< current index of array
+    int max = 0;        ///< high watermark for debugging
 
     List()  {
-    	v = N ? new T[N] : 0;                      /// dynamically allocate array storage
+    	v = N ? new T[N] : 0;                      ///< dynamically allocate array storage
     	if (!v) throw "ERR: List allot failed";
     }
-    ~List() { if (v) delete[] v;   }               /// free memory
+    ~List() { if (v) delete[] v;   }               ///< free memory
 
     List &operator=(T *a)   INLINE { v = a; return *this; }
     T    &operator[](int i) INLINE { return i < 0 ? v[idx + i] : v[i]; }
@@ -128,28 +128,28 @@ struct List {
 #if LAMBDA_OK
 struct fop { virtual void operator()() = 0; };
 template<typename F>
-struct XT : fop {           /// universal functor
+struct XT : fop {           ///< universal functor
     F fp;
     XT(F &f) : fp(f) {}
     void operator()() INLINE { fp(); }
 };
-typedef fop* FPTR;          /// lambda function pointer
+typedef fop* FPTR;          ///< lambda function pointer
 struct Code {
-    const char *name = 0;   /// name field
-    union {                 /// either a primitive or colon word
-        FPTR xt = 0;        /// lambda pointer
-        struct {            /// a colon word
-            U16 def:  1;    /// colon defined word
-            U16 immd: 1;    /// immediate flag
-            U16 len:  14;   /// len of pfa
-            IU  pfa;         /// offset to pmem space
+    const char *name = 0;   ///< name field
+    union {                 ///< either a primitive or colon word
+        FPTR xt = 0;        ///< lambda pointer
+        struct {            ///< a colon word
+            U16 def:  1;    ///< colon defined word
+            U16 immd: 1;    ///< immediate flag
+            U16 len:  14;   ///< len of pfa
+            IU  pfa;        ///< offset to pmem space
         };
     };
-    template<typename F>    /// template function for lambda
+    template<typename F>    ///< template function for lambda
     Code(const char *n, F f, bool im=false) : name(n), xt(new XT<F>(f)) {
         immd = im ? 1 : 0;
     }
-    Code() {}               /// create a blank struct (for initilization)
+    Code() {}               ///< create a blank struct (for initilization)
 };
 #define CODE(s, g) { s, []() { g; }}
 #define IMMD(s, g) { s, []() { g; }, true }
@@ -157,22 +157,22 @@ struct Code {
 ///
 /// a lambda without capture can degenerate into a function pointer
 ///
-typedef void (*FPTR)();     /// function pointer
+typedef void (*FPTR)();     ///< function pointer
 struct Code {
-    const char *name = 0;   /// name field
-    union {                 /// either a primitive or colon word
-        FPTR xt = 0;        /// lambda pointer
-        struct {            /// a colon word
-            U16 def:  1;    /// colon defined word
-            U16 immd: 1;    /// immediate flag
-            U16 xxx:  14;   /// reserved
-            IU  pfa;        /// offset to pmem space (16-bit for 64K range)
+    const char *name = 0;   ///< name field
+    union {                 ///< either a primitive or colon word
+        FPTR xt = 0;        ///< lambda pointer
+        struct {            ///< a colon word
+            U16 def:  1;    ///< colon defined word
+            U16 immd: 1;    ///< immediate flag
+            U16 xxx:  14;   ///< reserved
+            IU  pfa;        ///< offset to pmem space (16-bit for 64K range)
         };
     };
     Code(const char *n, FPTR f, bool im=false) : name(n), xt(f) {
         immd = im ? 1 : 0;
     }
-    Code() {}               /// create a blank struct (for initilization)
+    Code() {}               ///< create a blank struct (for initilization)
 };
 #define CODE(s, g) { s, []{ g; }}
 #define IMMD(s, g) { s, []{ g; }, true }
