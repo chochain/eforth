ec33bd85dd34a62b4d049c58820578507678431e
gnii <chochain@yahoo.com>
2024-08-28 12:34:22 +0800

    ~/tests/demo/17_array.fs add cells (backward compatible)

--- tests/demo/17_array.fs
+++ tests/demo/17_array.fs
@@ -1,6 +1,6 @@
 .( lesson 17 - array access )
 
-cr .( create arr 5 allot ) create arr 5 allot
+cr .( create arr 5 cells allot ) create arr 5 cells allot
 : st th ;
 : nd th ;
 : rd th ;
@@ -11,8 +11,10 @@ cr .( 111 arr 1 st ! ) 111 arr 1 st !
 cr .( 222 arr 2 nd ! ) 222 arr 2 nd !
 cr .( 333 arr 3 rd ! ) 333 arr 3 rd !
 cr .( 444 arr 4 th ! ) 444 arr 4 th ! cr
+
 cr .( see arr => ) cr see arr
 
+cr .( arr @      => ) arr @ .
 cr .( arr 1 st @ => ) arr 1 st @ .
 cr .( arr 2 nd @ => ) arr 2 nd @ .
 cr .( arr 3 rd @ => ) arr 3 rd @ .
