b94a89f4011d9c8605a470cff9b43d1fa1bdaeca
gnii <chochain@yahoo.com>
2024-11-04 17:12:31 -0800

    ~/orig/40x/ceforth_task.cpp implement multi-task support

--- orig/40x/ceforth_task.cpp
+++ orig/40x/ceforth_task.cpp
@@ -4,19 +4,16 @@
 ///
 #include "ceforth.h"
 
-VM      _vm0;                        ///< default task (if no pool)
-VM      *_vm;
-thread  *_pool;                      ///< std::thread
-int      _pool_sz = 0;
+extern void CALL(VM &vm, IU w);
 
-VM& vm_instance(int id) {
-    return id < _pool_sz ? _vm[id] : _vm0;
-}
-int vm_pool(int n) {
+VM    *_vm;
+int   _pool_sz = 0;
+
+int vm_pool_init(int n) {
     if (_pool_sz) return 0;             ///< already initailized
 
-    _vm = n > 1 ? (_pool = new thread[n], new VM[n]) : &_vm0;
-    if (!_vm || (n > 1 && !_pool)) {
+    _vm = new VM[n];
+    if (!_vm) {
         LOGS("VM and thread pool allocation failed\n");
         return 1;
     }
@@ -24,3 +21,32 @@ int vm_pool(int n) {
 
     return 0;
 }
+VM& vm_get(int id) {
+    return (id >= 0 && id < _pool_sz) ? _vm[id] : _vm[0];
+}
+
+#if DO_MULTITASK
+int vm_create(VM& vm0, IU xt) {
+    int i = 1;
+    while (i < _pool_sz && _vm[i].state != STOP) i++;
+    if (i >= _pool_sz) return 0;
+    
+    VM &vm1 = _vm[i];
+//    memcpy(&vm[i], &vm0, sizeof(VM));   /// duplicate stacks
+    vm1._ip = xt;
+
+    return i;
+}
+void vm_exec(int id) {
+    VM &vm = vm_get(id);
+    printf(">> thread[%d] started, vm.state=%d\n", id, vm.state);
+    vm.state = QUERY;
+    CALL(vm, vm._ip);
+    printf(">> thread[%d] done state=%d\n", id, vm.state);
+    vm.state = STOP;
+}
+void vm_start(int id) {
+    thread t(vm_exec, id);
+    t.detach();
+}
+#endif // DO_MULTITASK
