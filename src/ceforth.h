f561ecfba9636d2b300a377e2104322a58b8f095
gnii <chochain@yahoo.com>
2024-11-28 09:13:21 -0800

    ~/src/ceforth.h Code pointer => ref

--- src/ceforth.h
+++ src/ceforth.h
@@ -87,7 +87,7 @@ struct ALIGNAS VM {
 ///
 ///> data structure for dictionary entry
 ///
-typedef void (*XT)(VM &vm, Code*); ///< function pointer
+typedef void (*XT)(VM &vm, Code&);    ///< function pointer
 
 struct Code {
     const static U32 IMMD_FLAG = 0x80000000;
@@ -113,24 +113,25 @@ struct Code {
     ~Code() {}                                         ///> do nothing now
     
     Code *append(Code *w) { pf.push(w); return this; } ///> add token
-    void nest(VM &vm);                                 ///> inner interpreter
+//    void nest(VM &vm);                                 ///> inner interpreter
 };
 ///
 ///> Primitve object and function forward declarations
 ///
 struct Code;                         ///< Code class forward declaration
-typedef void (*XT)(VM&, Code*);      ///< function pointer
+typedef void (*XT)(VM&, Code&);      ///< function pointer
 
-void   _str(VM &vm, Code *c);        ///< dotstr, dostr
-void   _lit(VM &vm, Code *c);        ///< numeric liternal
-void   _var(VM &vm, Code *c);        ///< variable and constant
-void   _tor(VM &vm, Code *c);        ///< >r (for..next)
-void   _tor2(VM &vm, Code *c);       ///< swap >r >r (do..loop)
-void   _if(VM &vm, Code *c);         ///< if..then, if..else..then
-void   _begin(VM &vm, Code *c);      ///< ..until, ..again, ..while..repeat
-void   _for(VM &vm, Code *c);        ///< for..next, for..aft..then..next
-void   _loop(VM &vm, Code *c);       ///< do..loop
-void   _does(VM &vm, Code *c);       ///< does>
+void   _str(VM &vm, Code &c);        ///< dotstr, dostr
+void   _lit(VM &vm, Code &c);        ///< numeric liternal
+void   _var(VM &vm, Code &c);        ///< variable and constant
+void   _tor(VM &vm, Code &c);        ///< >r (for..next)
+void   _tor2(VM &vm, Code &c);       ///< swap >r >r (do..loop)
+void   _if(VM &vm, Code &c);         ///< if..then, if..else..then
+void   _begin(VM &vm, Code &c);      ///< ..until, ..again, ..while..repeat
+void   _for(VM &vm, Code &c);        ///< for..next, for..aft..then..next
+void   _loop(VM &vm, Code &c);       ///< do..loop
+void   _does(VM &vm, Code &c);       ///< does>
+void   _nest(VM &vm, Code &c);       ///< inner interpreter
 ///
 ///> polymorphic constructors
 ///
