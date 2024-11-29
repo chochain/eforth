6a88c03396b20b51840a6662957f1d82eae30551
gnii <chochain@yahoo.com>
2024-11-28 17:21:08 -0800

    ~/src/demo add mtask.fs (multi-threading)

---
+++ tests/demo/mtask.fs
@@ -0,0 +1,27 @@
+: xx 9999 for 34 drop next ;
+: yy 9999 for xx next ;
+: zz ms negate yy ms +
+  lock
+    ." VM" rank .
+    .
+    ." ms " cr
+  unlock ;
+' zz constant xt
+: ser ( n -- ) for zz next ;     \ tasks in serial 
+: par ( n -- )                   \ tasks in parallel
+  dup >r                           \ keep loop count
+  for
+    xt task dup start              \ start task, and keep task id as TOS
+  next
+  r> for join next ;               \ pop loop count and join tasks
+: bench ( fn n -- )              \ benchmark
+  ms negate >r
+  1- swap exec
+  r> ms + 
+  ." elapsed ms=" . cr ;
+.( 4 tasks run in serial ) cr
+' ser 4 bench
+.( 4 tasks run in parallel ) cr
+' par 4 bench
+bye
+
