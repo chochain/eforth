f5e5a2c0414550dd5ef4a9e2470ae781922962e3
gnii <chochain@yahoo.com>
2024-12-16 17:30:02 -0800

    ~/platform/eforth_vm0.js add mainScriptUrlOrBlob (control script to load, instead of pointing back)

--- platform/eforth_vm0.js
+++ platform/eforth_vm0.js
@@ -2,23 +2,26 @@
 /// @file
 /// @brief eForth - worker proxy to eforth.js (called by eforth.html)
 ///
-Module = { print: txt=>postMessage(txt) } ///> WASM print interface => output queue
+Module = {
+    mainScriptUrlOrBlob: 'eforth1.js',  ///< worker thread (emscriptened C)
+    print              : txt=>postMessage(txt)
+} ///> WASM print interface => output queue
 ///
 /// worker message pipeline to main thread
 ///
 var forth = null;
     
 onmessage = (e)=>{
-    console.warn("worker eforth_vm0.js handleMessage=", e.data);
-    if (e.data?.cmd === 'load') {
-        importScripts('eforth.js');    ///> load js emscripten created
+    console.warn("eforth_vm0.js: handleMessage=", e.data);
+    if (e.data?.cmd === 'init') {
+        importScripts(Module.mainScriptUrlOrBlob);  /// * load emscripten code
         forth = Module.cwrap('forth', null, ['number', 'string']);
         postMessage({ 'cmd' : 'ready'});
     }
     else {
-        var rst = forth(0, e.data);
+        var rst = forth(0, e.data);                 ///< process Forth command
         console.log(">>" + rst)
-        if (rst) postMessage(rst)      /// * send back response from Forth outer interpreter
+        if (rst) postMessage(rst)                   /// * send response to main
     }
 }
 
