0ea8b4ea4cd27dda12bffd02927ba2ff7e987ea5
Chochain Lee <chochain@yahoo.com>
2025-07-31 06:00:08 -0700

    ~/src/ceforth_sys.cpp pass Prim& to see

--- src/ceforth_sys.cpp
+++ src/ceforth_sys.cpp
@@ -115,7 +115,7 @@ void ss_dump(VM &vm, bool forced) {       ///> display data stack and ok promt
     TOS = SS.pop();
     fout << "-> ok" << ENDL;
 }
-void _see(const Code &c, int dp) {       ///> disassemble a colon word
+void _see(const Prim &c, int dp) {       ///> disassemble a colon word
     if (dp > 2) return;
     auto pp = [](const string &s, const FV<Code*> &pf, int dp) { ///> recursive dump with indent
         int i = dp;
@@ -129,9 +129,9 @@ void _see(const Code &c, int dp) {       ///> disassemble a colon word
     const FV<Code*> nil = {};
     string sn(c.name);
     if (c.is_str) sn = (c.token ? "s\" " : ".\" ") + sn + "\"";
-    pp(sn, c.pf, dp);
+    pp(sn, ((Code&)c).pf, dp);
     if (sn=="if")    {
-        if (c.stage==1) pp("else", ((Bran)c).p1, dp);
+        if (c.stage==1) pp("else", ((Bran&)c).p1, dp);
         pp("then", nil, dp);
     }
     else if (sn=="begin") {
@@ -139,24 +139,24 @@ void _see(const Code &c, int dp) {       ///> disassemble a colon word
         case 0: pp("until", nil, dp); break;
         case 1: pp("again", nil, dp); break;
         case 2:
-            pp("while",  ((Bran)c).p1, dp);
+            pp("while",  ((Bran&)c).p1, dp);
             pp("repeat", nil, dp);
             break;
         }
     }
     else if (sn=="for") {
         if (c.stage==3) {
-            pp("aft",  ((Bran)c).p1, dp);
-            pp("then", ((Bran)c).p2, dp);
+            pp("aft",  ((Bran&)c).p1, dp);
+            pp("then", ((Bran&)c).p2, dp);
         }
         pp("next", nil, dp);
     }
     else if (sn=="do") {
         pp("loop", nil, dp);
     }
-    else pq(c.q);
+    else pq(((Code&)c).q);
 }
-void see(const Code &c, int base) {
+void see(const Prim &c, int base) {
     if (c.xt) fout << "  ->{ " << c.desc << "; }";
     else {
         fout << ": "; _see(c, 0); fout << " ;";
@@ -213,8 +213,8 @@ void mem_dump(IU w0, IU w1, int base) {
         
         fout << w->name << ENDL;
         show_pf("pf", w->pf);
-        show_pf("p1", ((Bran)w)->p1);
-        show_pf("p2", ((Bran)w)->p2);
+        show_pf("p1", ((Bran*)w)->p1);
+        show_pf("p2", ((Bran*)w)->p2);
         
         if (w->q.size()==0) continue;
         fout << "  q:";
