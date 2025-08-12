0f9f5d8c82a636f7bc708c2a92f1b29149802e6b
Chochain Lee <chochain@yahoo.com>
2025-08-12 14:59:59 +0800

    ~/src/ceforth_task.cpp update uvar_init to use BASE_NODE (instead of 0)

--- src/ceforth_task.cpp
+++ src/ceforth_task.cpp
@@ -11,11 +11,12 @@ VM _vm0;                           ///< singleton, no VM pooling
 
 VM& vm_get(int id) { return _vm0; }/// * return the singleton
 void uvar_init() {
-    dict[0]->append(new Var(10));  /// * borrow dict[0]->pf[0]->q[vm.id] for VM's user area
+    Var *v = new Var(10);
+    dict[BASE_NODE]->append(v);    /// * borrow dict[0]->pf[0]->q[vm.id] for VM's user area
 
-    _vm0.id    = 0;                           /// * VM id
-    _vm0.state = HOLD;                        /// * VM ready to run
-    _vm0.base  = (U8*)&dict[0]->pf[0]->q[0];  /// * set base pointer
+    _vm0.id    = 0;                /// * VM id
+    _vm0.state = HOLD;             /// * VM ready to run
+    _vm0.base  = (U8*)&v->q[0];    /// * set base pointer
     *_vm0.base = 10;
 }
 
@@ -116,13 +117,13 @@ void t_pool_stop() {
 ///> setup/teardown user area (base pointer)
 ///
 void uvar_init() {
-    dict[0]->append(new Var(10));  /// * borrow dict[0]->pf[0]->q[vm.id] for VM's user area
+    Var *v = new Var(10);
+    dict[BASE_NODE]->append(v);  /// * borrow dict[0]->pf[0]->q[vm.id] for VM's user area
 
-    FV<DU> &q = dict[0]->pf[0]->q;
-    q.reserve(E4_VM_POOL_SZ);
+    v->q.reserve(E4_VM_POOL_SZ);
     for (int i = 0; i < E4_VM_POOL_SZ; i++) {
-        if (i > 0) q.push(10);                    /// * allocate next base storage
-        _vm[i].base = (U8*)&q[i];                 /// * set base pointer
+        if (i > 0) v->q.push(10);                 /// * allocate next base storage
+        _vm[i].base = (U8*)&v->q[i];              /// * set base pointer
         _vm[i].id   = i;                          /// * VM id
         _vm[i].reset(0, STOP);
     }
