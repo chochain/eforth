2a97190070af3c91b742e335a97a4959d2029d5c
Chochain Lee <chochain@yahoo.com>
2025-10-10 10:31:14 -0700

    ~/orig/50x,src/ceforth_task.cpp add __ANDROID__ filter (no affinity)

--- orig/50x/ceforth_task.cpp
+++ orig/50x/ceforth_task.cpp
@@ -89,7 +89,7 @@ void t_pool_init() {
         THREAD t(_event_loop, (int)_vm[i].id);    ///< create thread
         _pool[i] = std::move(t);                  /// * transfer thread ownership
 
-#if __has_include(<sched.h>) && !defined(__CYGWIN__)
+#if __has_include(<sched.h>) && !defined(__CYGWIN__) && !defined(__ANDROID__)
         pthread_t pt = _pool[i].native_handle();
         cpu_set_t set;
         CPU_ZERO(&set);                           /// * clear affinity
