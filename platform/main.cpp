d2c726183b298996063c884b63ba0a6334315dae
Chochain Lee <chochain@yahoo.com>
2025-10-28 11:48:24 -0700

    ~/platform/main.cpp replace C++ istream with C FILE; add getc so outer() became char-driven (instead of line-driven)

--- platform/main.cpp
+++ platform/main.cpp
@@ -2,19 +2,19 @@
 /// @file
 /// @brief eForth main program for testing on Desktop PC (Linux and Cygwin)
 ///
-#include <iostream>      // cin, cout
-#include <fstream>       // ifstream
-#include <cstdint>
-#include <thread>
-#include <chrono>
+#include <cstdint>         // U64
+#include <cstdio>          // standard IO
 
 #ifdef __APPLE__
 #include <sys/sysctl.h>
 #elif _WIN32 || _WIN64
 #include <windows.h>
 #include <string>
+#include <conio.h>         // getchar
 #else // Linux || Cygwin
-#include <sys/sysinfo.h>
+#include <sys/sysinfo.h>   // memory info
+#include <termios.h>       // getchar
+#include <unistd.h>        // STDIN_FILENO
 #endif
 
 using namespace std;
@@ -30,13 +30,13 @@ const char* APP_VERSION = "eForth v5.0";
 ///
 typedef uint64_t U64;
 void mem_stat() {
-    cout << APP_VERSION;
+    fprintf(stdout, "%s", APP_VERSION);
 
 #ifdef __APPLE__
     int64_t memsize;
     size_t len = sizeof(memsize);
     if (sysctlbyname("hw.memsize", &memsize, &len, NULL, 0) == 0) {
-      cout << ", RAM " << (memsize >> 20) << " MB";
+        fprintf(stdout, ", RAM %ld MB", memsize >> 20);
     }
 #elif _WIN32 || _WIN64
 	MEMORYSTATUSEX si;                        ///< Windows Memory Status
@@ -46,65 +46,101 @@ void mem_stat() {
 	    int p = si.dwMemoryLoad;              /// * percentage of memory in use
 		U64 f = (U64)si.ullAvailPhys;         /// * available physical memory
 		U64 t = (U64)si.ullTotalPhys;         /// * total physical memory
-        cout << ", RAM " << (100 - p) << "% free (" << (f >> 20)
-  			 << " / " << (t >> 20) << " MB)";
+        fprintf(stdout,
+                ", RAM %d%% free (%ld / %ld MB)",
+                100 - p, f >> 20, t >> 20)
     }
-	else cerr << "ERR: Windows memory status fetch failed!";
+	else fprintf(stderr, "ERR: Windows memory status fetch failed!");
 #else // Linux, Cygwin
     struct sysinfo si;
     if (sysinfo(&si) != -1) {
       U64 f = (U64)si.freeram * si.mem_unit;
       U64 t = (U64)si.totalram * si.mem_unit;
       U64 p = f * 1000L / t;
-      cout << ", RAM " << static_cast<float>(p) * 0.1 << "% free (" << (f >> 20)
-           << " / " << (t >> 20) << " MB)";
+      fprintf(stdout, 
+              ", RAM %3.1f%% free (%ld / %ld MB)",
+              static_cast<float>(p * 0.1), f >> 20, t >> 20);
     }
 #endif
 
-    cout << endl;
+    fprintf(stdout, "\n");
 }
 ///
 ///> include external Forth script
 ///
-#define TIB_SZ 128
-void outer(istream &in) {
+#if _WIN32 || _WIN64
+#else
+char getc(int file_no) {                      ///< get one unbuffered char with timeout
+	struct termios t0, t1;
+
+	tcgetattr(file_no, &t0);                  /// * backup stdin attributes
+	t1 = t0;
+	t1.c_lflag &= ~(ICANON | ECHO);           /// * non-buffered, and echo
+    t1.c_cc[VMIN]  = 0;                       /// * no waiting
+    t1.c_cc[VTIME] = 1;                       /// * timeout on 0.1 second (returns '\0')
+	tcsetattr(file_no, TCSANOW, &t1);         /// * set to non-buffered
+    
+	char ch;
+    int n = read(file_no, &ch, 1);            /// * fetch one char from given input file
+    
+	tcsetattr(file_no, TCSANOW, &t0);         /// * restore stdin attributes
+
+	return n ? ch: '\0';
+}
+#endif
+
+#define TIB_SZ 128                            /// * 128-byte line buffer
+void outer(int file_no) {
 	char  cmd[TIB_SZ+1];
-	int   idx = 0;
-	while (1) {
-		if (in.peek() == '\0') continue;      ///> polling
-		
-		char c = in.get();
-		printf(".%c", c);
-		if (c == 0x8) --idx;
-		else          cmd[idx < TIB_SZ ? idx++ : idx] = c;
-		if (c != '\n' && c != '\r') continue;
-		
-		cmd[idx] = '\0';
-		if (forth_vm(cmd)) break;             ///> run outer interpreter (single task)
+	int   idx  = 0;
+    int   done = 0;
+	while (!done) {
+        char c = getc(file_no);
+        switch (c) {
+        case '\0':
+            forth_vm("clock . cr 1000 ms");                   /// * handle timer interrupt
+            break;
+        case 0x8: --idx; break;               /// * backspace
+        case '\n': case '\r':
+            cmd[idx] = '\0';
+            done = forth_vm(cmd);             ///> run outer interpreter (single task)
+            idx  = 0;
+            break;
+        case EOF: 
+            printf(".EOF");
+            done = 1;
+            break;
+        default:                              /// * capture input char
+            printf(".%c", c);
+            cmd[idx < TIB_SZ ? idx++ : idx] = c;
+            break;
+        }
     }
 }
-void forth_include(const char *fn) {
-    ifstream ifile(fn);                       ///< open input stream
-    if (ifile.is_open()) {
-        outer(ifile);
-    }
-    else cout << "file " << fn << " open failed!";
 
-    ifile.close();
+void forth_include(const char *fn) {
+    FILE *fp = fopen(fn, "r");
+    
+    if (fp) outer(fp->_fileno);
+    else    fprintf(stderr, "failed to open file %s\n", fn);
+    
+    fclose(fp);
 }
 ///====================================================================
 ///
 /// main program - Note: Arduino and ESP32 have their own main-loop
 ///
+#include <cstdlib>                            /// srand
+#include <ctime>                              /// time
 int main(int ac, char* av[]) {
     forth_init();                             ///> initialize dictionary
     
     mem_stat();                               ///> show memory status
     srand((int)time(0));                      ///> seed random generator
-    outer(cin);                               ///> Forth outer interpreter
+    outer(STDIN_FILENO);                      ///> Forth outer interpreter
     
     forth_teardown();                         ///> clean up before we go
-    cout << APP_VERSION << " Done!" << endl;
+    fprintf(stdout, "%s Done!\n", APP_VERSION);
     return 0;
 }
 ///====================================================================
