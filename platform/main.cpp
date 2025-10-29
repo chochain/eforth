fc8b0c6825d1eb29bf44e0119e6eead2bc855d76
chochain <chochain@yahoo.com>
2025-10-28 23:08:45 -0700

    ~/platform/main.cpp update qkey for Windows platform

--- platform/main.cpp
+++ platform/main.cpp
@@ -36,18 +36,18 @@ void mem_stat() {
         fprintf(stdout, ", RAM %ld MB", memsize >> 20);
     }
 #elif _WIN32 || _WIN64
-	MEMORYSTATUSEX si;                        ///< Windows Memory Status
+    MEMORYSTATUSEX si;                        ///< Windows Memory Status
     si.dwLength = sizeof(si);                 /// * Initialize the structure
 
     if (GlobalMemoryStatusEx(&si)) {          /// * fetch from system
-	    int p = si.dwMemoryLoad;              /// * percentage of memory in use
-		U64 f = (U64)si.ullAvailPhys;         /// * available physical memory
-		U64 t = (U64)si.ullTotalPhys;         /// * total physical memory
+        int p = si.dwMemoryLoad;              /// * percentage of memory in use
+        U64 f = (U64)si.ullAvailPhys;         /// * available physical memory
+        U64 t = (U64)si.ullTotalPhys;         /// * total physical memory
         fprintf(stdout,
-                ", RAM %d%% free (%ld / %ld MB)",
-                100 - p, static_cast<long>(f >> 20), static_cast<long>(t >> 20))
+            ", RAM %d%% free (%ld / %ld MB)",
+            100 - p, static_cast<long>(f >> 20), static_cast<long>(t >> 20));
     }
-	else fprintf(stderr, "ERR: Windows memory status fetch failed!");
+    else fprintf(stderr, "ERR: Windows memory status fetch failed!");
 #else // Linux, Cygwin
     struct sysinfo si;
     if (sysinfo(&si) != -1) {
@@ -57,7 +57,7 @@ void mem_stat() {
       fprintf(stdout, 
               ", RAM %.1f%% free (%ld / %ld MB)",
               static_cast<float>(p * 0.1),
-			  static_cast<long>(f >> 20), static_cast<long>(t >> 20));
+              static_cast<long>(f >> 20), static_cast<long>(t >> 20));
     }
 #endif
     
@@ -69,7 +69,13 @@ void mem_stat() {
 #if _WIN32 || _WIN64
 #include <conio.h>         // getchar
 char qkey() {
-    return _kbhit() ? _getch() : '\0';
+    char c = _kbhit() ? _getche() : '\0';
+    switch (c) {
+    case 0x8:
+    case 0xff: putchar(' ');  putchar(c); break;
+    case '\r': putchar('\n'); break;
+    }
+    return c;
 }
 
 #else // _WIN32 || _WIN64
@@ -77,39 +83,40 @@ char qkey() {
 #include <unistd.h>        // STDIN_FILENO
 
 char qkey() {                                 ///< get one unbuffered char with timeout
-	struct termios t0, t1;
+    struct termios t0, t1;
 
     fflush(stdout);                           /// * flush output buffer before wait
-	tcgetattr(STDIN_FILENO, &t0);             /// * backup stdin attributes
-	t1 = t0;
-	t1.c_lflag &= ~(ICANON | ECHO);           /// * non-buffered, and echo
+    tcgetattr(STDIN_FILENO, &t0);             /// * backup stdin attributes
+    t1 = t0;
+    t1.c_lflag &= ~(ICANON | ECHO);           /// * non-buffered, and echo
     t1.c_cc[VMIN]  = 0;                       /// * capture 0 or more char
     t1.c_cc[VTIME] = 0;                       /// * 0: no wait, 1:timeout on 0.1 second (returns '\0')
-	tcsetattr(STDIN_FILENO, TCSANOW, &t1);    /// * set to non-buffered
-    
-	char c;
-    int n = read(STDIN_FILENO, &c, 1);        /// * fetch one char from given input file
-    
-	tcsetattr(STDIN_FILENO, TCSANOW, &t0);    /// * restore stdin attributes
+    tcsetattr(STDIN_FILENO, TCSANOW, &t1);    /// * set to non-buffered
 
-	return n ? c : '\0';
+    char c;
+    int n = read(STDIN_FILENO, &c, 1);        /// * fetch one char from given input file
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &t0);    /// * restore stdin attributes
+
+    return n ? c : '\0';
 }
 #endif // _WIN32 || _WIN64
 
 #define TIB_SZ 128                            /// * 128-byte line buffer
 void outer(FILE *fp) {
-	char cmd[TIB_SZ+1];
-	int  idx  = 0;
+    char cmd[TIB_SZ+1];
+    int  idx  = 0;
     int  done = 0;
-	while (!done) {
+    while (!done) {
         char c = (fp==stdin)                  ///< ?key or stream from file
-            ? qkey() : fgetc(fp);  
+            ? qkey() : fgetc(fp);
 //        fprintf(stderr, ".%c%x", c, c);
         switch (c) {
         case '\0':
             forth_vm(NULL);                   /// * handle timer interrupt
             break;
-        case 0x8: --idx;    break;            /// * backspace
+        case 0x8:                             /// * backspace
+        case 0xff: --idx;   break;            /// * erase
         case EOF: done = 1; break;            /// * done with input file
         case '\n': case '\r':
             cmd[idx] = '\0';
@@ -125,10 +132,10 @@ void outer(FILE *fp) {
 
 void forth_include(const char *fn) {
     FILE *fp = fopen(fn, "r");
-    
+
     if (fp) outer(fp);
     else    fprintf(stderr, "failed to open file %s\n", fn);
-    
+
     fclose(fp);
 }
 ///====================================================================
@@ -141,11 +148,11 @@ void forth_include(const char *fn) {
 int main(int ac, char* av[]) {
     std::ios_base::sync_with_stdio(true);     /// * sync C++ iostream with C stdio
     forth_init();                             ///> initialize dictionary
-    
+
     mem_stat();                               ///> show memory status
     srand((int)time(0));                      ///> seed random generator
     outer(stdin);                             ///> Forth outer interpreter
-    
+
     forth_teardown();                         ///> clean up before we go
     fprintf(stdout, "%s Done!\n", APP_VERSION);
     return 0;
