7522f1d78d6c75e07c4e3099614b4d4c141080c0
Chochain Lee <chochain@yahoo.com>
2025-12-26 22:55:13 -0800

    ~/platform/mqtt.h,cpp two-way pub/sub

--- platform/mqtt.cpp
+++ platform/mqtt.cpp
@@ -3,25 +3,12 @@
 ///
 #include "mqtt.h"
 
-#define CLIENTID    "gnii_mqtt"
-#define TOPIC       "qnii"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
+#define CLIENTID   "gnii_mqtt"
+#define QOS        1
+#define TIMEOUT    10000L
 
-void MQTT::_delivered(void *ctx, MQTTClient_deliveryToken token) {
+void MQTT::_delivered(void *ctx, mqtt_token_t token) {
     printf("Message with token value %d delivery confirmed\n", token);
-}
-
-int MQTT::_arrived(void *ctx, char *topic, int len, MQTTClient_message *msg) {
-    printf("Message arrived\n");
-    printf("     topic: %s\n", topic);
-    printf("   message: %.*s\n", msg->payloadlen, (char*)msg->payload);
-    
-    MQTTClient_freeMessage(&msg);
-    MQTTClient_free(topic);
-
-    return 1;
 }
 
 void MQTT::_conn_lost(void *ctx, char *cause) {
@@ -29,7 +16,9 @@ void MQTT::_conn_lost(void *ctx, char *cause) {
     if (cause) printf("     cause: %s\n", cause);
 }
 
-MQTT::MQTT(const char *uri) {
+MQTT::MQTT(const char *uri, const char *topic_put, const char *topic_get, int (*callback)(void*, char*, int, mqtt_msg_t*)) {
+    _topic_put = topic_put;
+    _topic_get = topic_get;
     int rc;
     if ((rc = MQTTClient_create(
              &_rcvr, uri, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS) {
@@ -43,21 +32,69 @@ MQTT::MQTT(const char *uri) {
     }
     if ((rc = MQTTClient_setCallbacks(
              _rcvr, NULL,
-             MQTT::_conn_lost, MQTT::_arrived, MQTT::_delivered)) != MQTTCLIENT_SUCCESS) {
+             MQTT::_conn_lost, callback, MQTT::_delivered)) != MQTTCLIENT_SUCCESS) {
         printf("rcvr: Failed to set callbacks, return code %d\n", rc);
         goto bail;
     }
-    rc = 0;
+    rc = _connect();
 bail:
     _status = rc;
 }
 
 MQTT::~MQTT() {
+    _disconnect();
     MQTTClient_destroy(&_rcvr);
     MQTTClient_destroy(&_sndr);
 }
 
-int MQTT::connect() {
+#include <cstring>
+int MQTT::publish(void *payload) {
+    mqtt_msg_t msg = MQTT_MSG_INIT;
+    
+    msg.payload    = payload;
+    msg.payloadlen = (int)strlen((char*)payload);
+    msg.qos        = QOS;
+    msg.retained   = 0;
+
+    int rc;
+    if ((rc = MQTTClient_publishMessage(_sndr, _topic_put, &msg, &_token)) != MQTTCLIENT_SUCCESS) {
+         printf("sndr: Failed to publish message, return code %d\n", rc);
+         goto bail;
+    }
+    printf("sndr: Waiting for up to %d seconds for publication of %s\n"
+            "on topic %s for client with ClientID: %s\n",
+           (int)(TIMEOUT/1000), (char*)payload, _topic_put, CLIENTID);
+    
+    rc = MQTTClient_waitForCompletion(_sndr, _token, TIMEOUT);
+    printf("Message with delivery token %d delivered\n", _token);
+    
+bail:
+    return 0;
+}
+
+int MQTT::subscribe() {
+    int rc;
+    if ((rc = MQTTClient_subscribe(_rcvr, _topic_get, QOS)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to subscribe, return code %d\n", rc);
+        goto bail;
+    }
+    else {
+        int ch;
+        do {
+            ch = getchar();
+        } while (ch!='Q' && ch != 'q');
+
+        if ((rc = MQTTClient_unsubscribe(_rcvr, _topic_get)) != MQTTCLIENT_SUCCESS) {
+            printf("rcvr: Failed to unsubscribe, return code %d\n", rc);
+            goto bail;
+        }
+    }
+    rc = 0;
+bail:    
+    return rc;
+}
+
+int MQTT::_connect() {
     _opts.keepAliveInterval = 20;
     _opts.cleansession = 1;
     
@@ -70,61 +107,12 @@ int MQTT::connect() {
         printf("sndr: Failed to connect, return code %d\n", rc);
         goto bail;
     }
-    printf("rcvr: Subscribing to topic %s\nfor %s using QoS%d\n\n"
-           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
     rc = 0;
 bail:
     return rc;
 }
 
-#include <cstring>
-int MQTT::publish(void *payload) {
-    MQTTClient_message msg = MQTTClient_message_initializer;
-    
-    msg.payload    = payload;
-    msg.payloadlen = (int)strlen(PAYLOAD);
-    msg.qos        = QOS;
-    msg.retained   = 0;
-
-    int rc;
-    if ((rc = MQTTClient_publishMessage(_sndr, TOPIC, &msg, &_token)) != MQTTCLIENT_SUCCESS) {
-         printf("sndr: Failed to publish message, return code %d\n", rc);
-         goto bail;
-    }
-    printf("sndr: Waiting for up to %d seconds for publication of %s\n"
-            "on topic %s for client with ClientID: %s\n",
-           (int)(TIMEOUT/1000), (char*)payload, TOPIC, CLIENTID);
-    
-    rc = MQTTClient_waitForCompletion(_sndr, _token, TIMEOUT);
-    printf("Message with delivery token %d delivered\n", _token);
-    
-bail:
-    return 0;
-}
-
-int MQTT::subscribe() {
-    int rc;
-    if ((rc = MQTTClient_subscribe(_rcvr, TOPIC, QOS)) != MQTTCLIENT_SUCCESS) {
-        printf("Failed to subscribe, return code %d\n", rc);
-        goto bail;
-    }
-    else {
-        int ch;
-        do {
-            ch = getchar();
-        } while (ch!='Q' && ch != 'q');
-
-        if ((rc = MQTTClient_unsubscribe(_rcvr, TOPIC)) != MQTTCLIENT_SUCCESS) {
-            printf("rcvr: Failed to unsubscribe, return code %d\n", rc);
-            goto bail;
-        }
-    }
-    rc = 0;
-bail:    
-    return rc;
-}
-
-int MQTT::disconnect() {
+int MQTT::_disconnect() {
     int rc;
     if ((rc = MQTTClient_disconnect(_rcvr, TIMEOUT)) != MQTTCLIENT_SUCCESS) {
         printf("Failed to disconnect, return code %d\n", rc);
--- platform/mqtt.h
+++ platform/mqtt.h
@@ -3,22 +3,31 @@
 ///
 #include "MQTTClient.h"
 
-class MQTT {
-    MQTTClient_connectOptions _opts = MQTTClient_connectOptions_initializer;
-    MQTTClient                _sndr, _rcvr;
-    int                       _status;
-    int                       _token;
+typedef MQTTClient_message        mqtt_msg_t;
+typedef MQTTClient_deliveryToken  mqtt_token_t;
+#define MQTT_CONN_INIT            MQTTClient_connectOptions_initializer
+#define MQTT_MSG_INIT             MQTTClient_message_initializer        
 
-public:    
-    static void _delivered(void *ctx, MQTTClient_deliveryToken dt);
-    static int  _arrived(void *ctx, char *topic, int len, MQTTClient_message *msg); /// message arrived(topic_name, len)
-    static void _conn_lost(void *ctx, char *cause);                                 /// connection lost
+class MQTT {
+    MQTTClient_connectOptions _opts = MQTT_CONN_INIT;
+    MQTTClient _sndr;
+    MQTTClient _rcvr;
+    int        _status;
+    int        _token;
+    const char *_topic_put;
+    const char *_topic_get;
+
+public:
+    static void _delivered(void *ctx, mqtt_token_t dt);
+    static void _conn_lost(void *ctx, char *cause);                         /// connection lost
     
-    MQTT(const char *uri);
+    MQTT(const char *uri, const char *topic_put, const char *topic_get, int (*callback)(void*, char*, int, mqtt_msg_t*));
     ~MQTT();
     
-    int connect();
     int subscribe();
     int publish(void *payload);
-    int disconnect();
+
+private:    
+    int _connect();
+    int _disconnect();
 };
