f21a8694c821afa0abc32dce7d6017dfbe3df72b
Chochain Lee <chochain@yahoo.com>
2025-12-28 14:41:44 -0800

    ~/platform/mqtt.h contructor gets get_hndl/put_hndl; add _setup

--- platform/mqtt.h
+++ platform/mqtt.h
@@ -5,19 +5,24 @@
 ///
 #include "MQTTClient.h"
 
+typedef MQTTClient                mqtt_t;
 typedef MQTTClient_message        mqtt_msg_t;
+typedef MQTTClient_connectOptions mqtt_opts_t;
 typedef MQTTClient_deliveryToken  mqtt_token_t;
+typedef int (*mqtt_hndl)(void*, char*, int, mqtt_msg_t*); ///< message handler
+
 #define MQTT_CONN_INIT            MQTTClient_connectOptions_initializer
 #define MQTT_MSG_INIT             MQTTClient_message_initializer        
 
 class MQTT {
-    MQTTClient_connectOptions _opts = MQTT_CONN_INIT;
-    MQTTClient _sndr;
-    MQTTClient _rcvr;
-    int        _status;
-    int        _token;
-    const char *_topic_put;
-    const char *_topic_get;
+    mqtt_opts_t _opts = MQTT_CONN_INIT;
+    const char  *_topic_put;
+    const char  *_topic_get;
+    
+    mqtt_t      _sndr;
+    mqtt_t      _rcvr;
+    int         _status;
+    int         _token;
 
 public:
     static void _delivered(void *ctx, mqtt_token_t dt);   /// message delivery notifier
@@ -25,13 +30,15 @@ public:
     
     MQTT(const char *uri,
          const char *topic_get,
-         int (*hndl)(void*, char*, int, mqtt_msg_t*),
-         const char *topic_put);
+         mqtt_hndl  get_hndl,
+         const char *topic_put,
+         mqtt_hndl  put_hndl);
     ~MQTT();
     
     int publish(char *payload);
 
-private:    
+private:
+    int _setup(const char *id, const char *rui, mqtt_t *node, mqtt_hndl hndl);
     int _subscribe();
     int _connect();
     int _disconnect();
