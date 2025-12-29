55d82489dba3d096d7407ebcc3d59633d51a92d9
Chochain Lee <chochain@yahoo.com>
2025-12-28 21:42:12 -0800

    ~/platform/mqtt.cpp add wait for async completion

--- platform/mqtt.cpp
+++ platform/mqtt.cpp
@@ -2,6 +2,7 @@
 /// @file
 /// @brief - MQTT agent class
 ///
+#include <unistd.h>
 #include "mqtt.h"
 
 #define CLINETID   "gnii_mqtt"
@@ -12,6 +13,9 @@ void MQTT::_conn_lost(void *ctx, char *cause) {
     printf("\nConnection lost\n");
     if (cause) printf("     cause: %s\n", cause);
 }
+
+int MQTT::_conn_status = 0;
+int MQTT::_sub_status  = 0;
 
 MQTT::MQTT(
     const char *uri,            ///< URL of MQTT broker
@@ -25,15 +29,16 @@ MQTT::MQTT(
     _topic_put = topic_put;
     _topic_get = topic_get;
     
-    _status =
-        _connect("rcvr", uri, &_rcvr, get_hndl) ||
-//      _connect("sndr", uri, &_sndr, put_hndl) ||
-        _subscribe();
+    _connect("rcvr", uri, &_rcvr, get_hndl);
+//  _connect("sndr", uri, &_sndr, put_hndl);
+    _subscribe();
 }
 
 MQTT::~MQTT() {
     _disconnect();
-    
+    ///
+    /// wait for disconnected
+    ///
     MQTTAsync_destroy(&_rcvr);
 //    MQTTClient_destroy(&_sndr);
 }
@@ -68,7 +73,7 @@ int MQTT::_connect(const char *id, const char *uri, mqtt_t *node, mqtt_hndl hndl
         return rc;
     }
     if ((rc = MQTTAsync_setCallbacks(
-             *node, *node, MQTT::_conn_lost, hndl, NULL)) != MQTTASYNC_SUCCESS) {
+             *node, *node, _conn_lost, hndl, NULL)) != MQTTASYNC_SUCCESS) {
         printf("%s: Failed to set callbacks, return code %d\n", id, rc);
         return rc;
     }
@@ -84,19 +89,19 @@ int MQTT::_connect(const char *id, const char *uri, mqtt_t *node, mqtt_hndl hndl
         printf("%s: Failed to connect, return code %d\n", id, rc);
         return rc;
     }
+
+    while (_conn_status != 1) usleep(10000L);
     return 0;
 }
 
 int MQTT::_disconnect() {
     mqtt_disconn_opts_t opts = MQTT_DISCONN_INIT;
-    opts.context   = _rcvr;
-    opts.timeout   = TIMEOUT;
     opts.onSuccess = _disconn_ok;
     opts.onFailure = _disconn_err;
     
     int rc;
 /*    
-    if ((rc = MQTTAsync_disconnect(_sndr, TIMEOUT)) != MQTTASYNC_SUCCESS) {
+    if ((rc = MQTTAsync_disconnect(_sndr, &opts)) != MQTTASYNC_SUCCESS) {
     	printf("Failed to disconnect, return code %d\n", rc);
         return rc;
     }
@@ -105,6 +110,8 @@ int MQTT::_disconnect() {
         printf("Failed to disconnect, return code %d\n", rc);
         return rc;
     }
+
+    while (_conn_status != 0) usleep(10000L);
     return 0;
 }
 
@@ -120,6 +127,8 @@ int MQTT::_subscribe() {
         return rc;
     }
     printf("successfully subscribe to %s\n", _topic_get);
+
+    while (_sub_status != 1) usleep(10000L);
     return 0;
 }
 
