e94b724c9491cf6e0b4a80e1fc5256503a09fa7d
Chochain Lee <chochain@yahoo.com>
2026-09-03 20:35:09 -0700

    ~/orig/50x/config.h add IBUF_SZ,OBUF_SZ

--- orig/50x/config.h
+++ orig/50x/config.h
@@ -11,7 +11,7 @@
 #define USE_FLOAT       0               /**< support floating point */
 #define DO_MULTITASK    0               /**< multitasking/pthread   */
 #define DO_WASM         __EMSCRIPTEN__  /**< for WASM output        */
-#define SIM_TIMER_INTR  (!DO_MULTITASK) /**< fake timer interrupt   */
+#define SIM_TIMER_INTR  0               /**< fake timer interrupt   */
 ///@}
 ///@name Memory block configuation
 ///@{
@@ -20,6 +20,8 @@
 #define E4_DICT_SZ      400
 #define E4_PMEM_SZ      (32*1024)
 #define E4_VM_POOL_SZ   8               /**< one plus # cors       */
+#define E4_IBUF_SZ      128             /**< input buffer size     */
+#define E4_OBUF_SZ      1024            /**< output buffer size    */
 ///@}
 ///
 ///@name Logical units (instead of physical) for type check and portability
